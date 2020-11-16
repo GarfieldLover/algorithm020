cfbd8d83fff2140c29c293301287d83f4eebe031
Ke Zhang <zhangke19880503@163.com>
2020-11-16 22:00:25 +0800

    创建工程

---
+++ Week1/Work1/Work1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Work1
+//
+//  Created by zhangke on 2020/11/16.
+//  Copyright © 2020 Interview. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
